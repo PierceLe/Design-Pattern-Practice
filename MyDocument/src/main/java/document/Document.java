0900d50fd194cb0dabd262c810dd4e1dd99b074b
PierceLe <haichau.hvsg@gmail.com>
2024-09-07 18:29:54 +1000

    build: make src file for the scaffold of the project

---
+++ MyDocument/src/main/java/document/Document.java
@@ -0,0 +1,21 @@
+package document;
+
+public class Document {
+    private String text = "";
+    private final double margin;
+    private final String author;
+
+    public Document(double margin, String author) {
+        this.margin = margin;
+        this.author = author;
+    }
+
+    @Override
+    public String toString(){
+        return this.text;
+    }
+
+    public void append(String text){
+        this.text += text + "\n";
+    }
+}
---
+++ MyDocument/src/main/java/document/DocumentSystem.java
@@ -0,0 +1,68 @@
+package document;
+
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class DocumentSystem {
+
+    // DO NOT MODIFY
+    private static final String FOLDER_NAME = "output";
+
+    public static Document createDocument(){
+        return null;
+    }
+
+    public static void append(Document document, String text) {
+
+    }
+
+    public static void undo(Document document) {
+
+    }
+
+    public static void redo(Document document) {
+
+    }
+
+    public static void main(String[] args) {
+        // DO NOT MODIFY
+        test();
+    }
+
+    public static void test(){
+        // DO NOT MODIFY
+        Document document = createDocument();
+        File folder = new File(FOLDER_NAME);
+        folder.mkdir();
+
+        int i = 0;
+
+        while (i < 3){
+            append(document, String.format("Line %d", i));
+            i += 1;
+        }
+
+        writeDocumentToFile(document, String.format("original.txt", i));
+
+        undo(document);
+        writeDocumentToFile(document, "1-undo.txt");
+        undo(document);
+        writeDocumentToFile(document, "2-undo.txt");
+
+        redo(document);
+        writeDocumentToFile(document, "3-redo.txt");
+        redo(document);
+        writeDocumentToFile(document, "4-redo.txt");
+    }
+
+
+    public static void writeDocumentToFile(Document document, String filename){
+        // DO NOT MODIFY
+        try (FileWriter writer = new FileWriter(FOLDER_NAME + "/" + filename)) {
+            writer.write(document.toString());
+        } catch (IOException e) {
+            e.printStackTrace(); // Handle the exception
+        }
+    }
+}
